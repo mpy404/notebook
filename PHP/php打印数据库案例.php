5093fd548ef54ba900304fbfe74411d313189f20
mpy404 <3035277443@qq.com>
2020-02-08 18:20:52 +0800

    修改

--- PHP/php打印数据库案例.php
+++ PHP/php打印数据库案例.php
@@ -7,14 +7,11 @@ $tablename = "family";
 // 数据库操作
 $result = mysqli_query($conn,"select * from " . $tablename);
 $counts = mysqli_query($conn,"select count(*) from information_schema.COLUMNS where table_name = " . $tablename);
-echo $counts;
 // 打印每个数据
-while($row = $result->fetch_row())
-{
+while($row = mysqli_fetch_row($result)){
     for ($i=0; $i < 3; $i++) { 
-        echo "&emsp;&emsp;"."$row[$i]"."&emsp;&emsp;"; //这里是不是一行，放4个要显示的值不就行了？
+        echo "&emsp;&emsp;"."$row[$i]"."&emsp;&emsp;";
     }
     echo "<br />";
 }
-
 ?>
