df894ea01025146033faf4389d111ccc6769ab73
mpy404 <3035277443@qq.com>
2020-02-08 18:14:00 +0800

    php导出数据库

--- PHP/php打印数据库案例.php
+++ PHP/php打印数据库案例.php
@@ -1,9 +1,13 @@
 <?php
 // 数据库连接
 $conn = mysqli_connect("127.0.0.1","root","root");
+// 修改数据据库名和表名
 mysqli_select_db($conn,"mpy");
+$tablename = "family";
 // 数据库操作
-$result = mysqli_query($conn,"select * from family");
+$result = mysqli_query($conn,"select * from " . $tablename);
+$counts = mysqli_query($conn,"select count(*) from information_schema.COLUMNS where table_name = " . $tablename);
+echo $counts;
 // 打印每个数据
 while($row = $result->fetch_row())
 {
